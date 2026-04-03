964ea54f4464d8953d22602c17dc0fe66d5a7a31
SUKIYAKI-NEKO <wasdcx0905@gmail.com>
2026-04-03 20:25:20 +0800

    context

--- LeetCode/Two sum.cpp
+++ LeetCode/Two sum.cpp
@@ -1,9 +1,26 @@
 /*
 Two Sum
-source:
-context:
+source: https://leetcode.com/problems/two-sum/description/
+context: hash
 */
 
+//first:
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int a,b;
+        for(a=0;a<nums.size();a++){
+            for(b=0;b<nums.size();b++){
+                if(a==b) continue;
+                if(nums[a]+nums[b]== target)return {a,b};
+            }
+        }
+        return {};
+    }
+};
+//Runtime:144ms  Memory:14.15MB
+
+//second:
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -16,3 +33,4 @@ public:
         return {};
     }
 };
+//Runtime:1ms  Memory:14.89MB
