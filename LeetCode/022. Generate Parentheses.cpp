7d2f48578d23a4b736a898a788ea94582cd750e5
すき焼き <wasdcx0905@gmail.com>
2026-09-13 11:39:28 +0800

    Create 022. Generate Parentheses.cpp

---
+++ LeetCode/022. Generate Parentheses.cpp
@@ -0,0 +1,57 @@
+// 09/03/2026
+// https://leetcode.com/problems/generate-parentheses/description/
+
+// Medium
+
+// Topics:
+// String 、 Dynamic Programming 、 Backtracking 、 Bracket Sequences
+
+// Description:
+// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+// Submission:
+// Accepted
+// Original solution - written by me
+// Approach : Backtracking
+// Runtime: 0 ms  Memory: 13.40 MB
+
+
+// Submission:
+
+class Solution {
+public:
+    string s;
+    vector<string>v;
+    void generateback(int a,int b,int n,int nums){
+        if(s.length()==n*2){
+            v.push_back(s);
+            nums++;
+            return;
+        }
+        if(a<n){
+            s+='(';
+            a++;
+            generateback(a,b,n,nums);
+            s.pop_back();
+            a--;
+        }
+        if(b<a){
+            s+=')';
+            b++;
+            generateback(a,b,n,nums);
+            s.pop_back();
+            b--;
+        }
+        return;
+    }
+    vector<string> generateParenthesis(int n) {
+        generateback(0,0,n,0);
+        return v;
+    }
+};
+
+
+
+
+
+// end
